755ed4051f20536ff17d447f056bc243e85d6197
Leonard G <135045173+Leonardo5Gc@users.noreply.github.com>
2023-08-26 11:06:17 -0500

    Delete ejer1f.py

--- lab 05/ejer1f.py
+++
@@ -1,9 +0,0 @@
-edades = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24]
-valorMin = min(edades)
-valorMax = max(edades)
-promedio = (sum(edades) / len(edades))
-resultA = (valorMin - promedio), abs(valorMin - promedio)
-resultB = (valorMax - promedio), abs(valorMax - promedio) 
-
-print(resultA)
-print(resultB)
