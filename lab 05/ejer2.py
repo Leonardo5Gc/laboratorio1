fff1a6517ff00a7d10522129bbf834e1b5ecd22c
Leonard G <135045173+Leonardo5Gc@users.noreply.github.com>
2023-08-26 12:09:18 -0500

    Update ejer2.py

--- lab 05/ejer2.py
+++ lab 05/ejer2.py
@@ -2,4 +2,4 @@ palabra = ("Soy profesor y me encanta inspirar y enseñar a la gente")
 palabras = palabra.split()
 palabrasUnicas = set(palabras)
 
-print("Las palbra unica de la frase son:",len(palabrasUnicas))
+print("Las palabra unica de la frase son:",len(palabrasUnicas))
